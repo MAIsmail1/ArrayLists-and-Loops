bb40cee196fec1bd06efc6660acb142085ff8630
Mohamed Ismail <mohamedaismail95@gmail.com>
2022-04-20 00:02:58 +0100

    on the last question

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -25,12 +25,15 @@ public class Runner {
 //        5. Remove "Tresco" from the list by name
         scottishIslands.remove("Tresco");
 //        6. Remove "Arran" from the list by index
+        scottishIslands.remove(5);
 //        7. Print the number of islands in your arraylist
-//        8. Sort the list alphabetically
+        System.out.println(scottishIslands.size());
+//        8. Sort the list alphabetical
+       Collections.sort(scottishIslands);
 //        9. Print out all the islands using a for loop
-
-        System.out.println(scottishIslands);
-
+        for (String island : scottishIslands) {
+            System.out.println(island);
+        }
 //        NUMBERS
         List<Integer> numbers = new ArrayList<>();
         Collections.addAll(numbers, 1, 1, 4, 2, 7, 1, 6, 15, 13, 99, 7);
@@ -38,15 +41,35 @@ public class Runner {
         System.out.println("numbers: " + numbers);
 
 //        1. Print out a list of the even integers
+        List<Integer> evenNumbers = new ArrayList<>();
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                evenNumbers.add(number);
+            }
+        }
+        System.out.println("even numbers are: " + evenNumbers);
 //        2. Print the difference between the largest and smallest value
+        int maxNumber = Collections.max(numbers);
+        int minNumber = Collections.min(numbers);
+        int difference = maxNumber - minNumber;
+        System.out.println("difference between min and max is: " + difference);
 //        3. Print true if the list contains a 1 next to a 1 somewhere
+        if(numbers.contains(1)) {
+            System.out.println(true);
+        }
 //        4. Print the sum of the numbers
+        int sumOfNumbers = 0;
+        for (int number : numbers) {
+            sumOfNumbers += number;
+        }
+        System.out.println("The sum of the numbers are: " + sumOfNumbers);
 //        5. Print the sum of the numbers...
 //           ...except the number 13 is unlucky, so it does not count...
 //           ...and numbers that come immediately after a 13 also do not count
 //          HINT - You will need to track the index throughout the loop
 //
 //          So [7, 13, 2] would have sum of 9.
+
 
     }
 
